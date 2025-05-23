64209da5b1be08b31289b7b3e9efde62f6eee447
yash gupta <141262414+yashtech00@users.noreply.github.com>
2025-05-23 21:27:08 +0530

    update

--- client/src/components/Events.tsx
+++ client/src/components/Events.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { EventComp } from "./EventComp";
@@ -40,51 +39,6 @@ export const Events = () => {
               <EventComp event={event} />
             </motion.div>
           ))}
-=======
-import { useMutation, useQuery } from "@tanstack/react-query";
-import axios from "axios";
-import { useState } from "react"
-import { EventComp } from "./EventComp";
-
-export interface EventProp{
-    title: string,
-    description: string,
-    category: string,
-    deadline:Date
-}
-
-
-
-export const Events = () => {
-
-
-    const BACKEND_URL = import.meta.env.BACKEND_URL;
-    const { data: Events } = useQuery<EventProp[]>({
-        queryKey: ["event"],
-        queryFn: async () => {
-            try {
-                const res = await axios.get(`${BACKEND_URL}/event`,{withCredentials:true});
-                return res.data
-            } catch (e:any) {
-                console.error(e.message);
-            }
-        },
-   })
-
-
-    return (
-        <div>
-            <div>
-                <p>All Events</p>
-                <div>
-                    {Events?.map((event) => (
-                        <div>
-                            <EventComp event={ event} />
-                    </div>
-                ))}
-                </div>
-                </div>
->>>>>>> 64e39949327f6d72f8df10dd6b7552febe0b461c
         </div>
       </div>
     </>
